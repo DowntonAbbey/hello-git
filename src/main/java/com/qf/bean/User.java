249102fa92400d4b6c3154404d5d4c5078b19644
houshangkun <1318680455@qq.com>
2018-09-17 20:09:36 +0800

    第二次提交

--- src/main/java/com/qf/bean/User.java
+++ src/main/java/com/qf/bean/User.java
@@ -6,4 +6,35 @@ public class User {
 	private String password;
 	private String sex;
 	private String age;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
 }
